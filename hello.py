9a372684a406d87f2073961879efd5f9b9c19d1a
Orien Zeng <ozeng@stanford.edu>
2016-02-13 14:51:22 -0800

    implement getting songs from spotify api

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 """Cloud Foundry test"""
-from flask import Flask, request
+from __future__ import print_function # In python 2.7
+from flask import Flask, request, redirect
+import sys
 import os
+import json
 import requests
 
 app = Flask(__name__)
@@ -31,11 +34,25 @@ def home():
 
 @app.route("/getToken")
 def getToken():
-	return requests.get("https://accounts.spotify.com/authorize/?client_id=b4f179be39ec4098b5b972cdad7f03fb&response_type=code&redirect_uri=https://throwback.mybluemix.net/getCode&scope=playlist-read-private%20user-library-read").content
+	return redirect("https://accounts.spotify.com/authorize/?client_id=b4f179be39ec4098b5b972cdad7f03fb&response_type=code&redirect_uri=https://throwback.mybluemix.net/getCode&scope=playlist-read-private%20user-library-read", code=302)
 
 @app.route("/getCode")
 def getCode():
-	return request.post("https://accounts.spotify.com/api/token", data = {"code": request.args['code'], "grant_type": "authorization_code", "redirect_uri": "https://throwback.mybluemix.net/getCode", "client_id": "b4f179be39ec4098b5b972cdad7f03fb", "client_secret": "c951d664306649d4a29f4411e1a9c56d"})
+	return request.args['code']
+	return requests.post("https://accounts.spotify.com/api/token", data = {"code": request.args['code'], "grant_type": "authorization_code", "redirect_uri": "https://throwback.mybluemix.net/getCode", "client_id": "b4f179be39ec4098b5b972cdad7f03fb", "client_secret": "c951d664306649d4a29f4411e1a9c56d"})
+
+@app.route("/getSongs")
+def getSongs():
+	jsonSongs = requests.get("https://api.spotify.com/v1/me/tracks?access_token=" + request.args['spotify_token']).content
+	dictionary = json.loads(jsonSongs)
+	songs = dictionary['items']
+	songsParsed = []
+	for song in songs:
+		songsParsed.append({"added_at":song['added_at'], "id":song['track']['id'], 'name':song['track']['name']})
+		print(song["added_at"], file=sys.stderr) 
+		print(song['track']['id'], file=sys.stderr)
+		print(song['track']['name'], file=sys.stderr)
+	return str(songsParsed)
 
 @app.route("/request", methods = ["GET"])
 def process_request():
